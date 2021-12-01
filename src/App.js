73e2c96aec9339a33e645c5193ea8acc39d4bdc9
natkilima <nathalie.rognon.juyn@gmail.com>
2021-12-01 21:53:50 +0000

    react and axios added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Coucou le monde</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
